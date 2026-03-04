8469b5671cda287ab2ee6ef331271ec088bcc6a8
Joe Yates <joe.g.yates@gmail.com>
2026-03-04 12:45:49 +0100

    Wrap baseline check with mix setup and cleanup

--- lib/green_validation/baseline_formatter.ex
+++ lib/green_validation/baseline_formatter.ex
@@ -3,7 +3,8 @@ defmodule GreenValidation.BaselineFormatter do
   Performs baseline formatting checks on projects without Green formatter.
   """
 
-  alias GreenValidation.{GreenInstaller, Project}
+  alias GreenValidation.Project
+  alias GreenValidation.Installer.MixExs
 
   @doc """
   Checks if a project is already formatted according to Elixir's standard formatter.
@@ -13,25 +14,34 @@ defmodule GreenValidation.BaselineFormatter do
   @spec ensure_clean(Project.t()) ::
           {:ok, :clean | :created_format_commit} | {:error, String.t()}
   def ensure_clean(%Project{} = project) do
-    GreenInstaller.prepare_formatter_exs(project)
+    MixExs.ensure_mix_exs(project)
     project_path = Project.path(project)
 
-    case System.cmd("mix", ["format", "--check-formatted"],
-           cd: project_path,
-           stderr_to_stdout: true
-         ) do
-      {_output, 0} ->
-        {:ok, :clean}
+    try do
+      case System.cmd("mix", ["format", "--check-formatted"],
+              cd: project_path,
+              stderr_to_stdout: true
+            ) do
+        {_output, 0} ->
+          {:ok, :clean}
 
-      {_output, 1} ->
-        with :ok <- format(project),
-             :ok <- commit_format_changes(project) do
-          {:ok, :created_format_commit}
-        end
+        {_output, 1} ->
+          with :ok <- format(project),
+               :ok <- cleanup(project),
+               :ok <- commit_format_changes(project) do
+            {:ok, :created_format_commit}
+          end
 
-      {output, exit_code} ->
-        {:error, "mix format failed with exit code #{exit_code}: #{output}"}
+        {output, exit_code} ->
+          {:error, "mix format failed with exit code #{exit_code}: #{output}"}
+      end
+    after
+      :ok = cleanup(project)
     end
+  end
+
+  defp cleanup(project) do
+    :ok = MixExs.reset_mix_exs(project)
   end
 
   @spec format(Project.t()) :: :ok | {:error, String.t()}
