2046b0872fc6d06df5ea3e0d9963ca5f8c7f62a4
Joe Yates <joe.g.yates@gmail.com>
2026-03-06 09:53:29 +0100

    Enable check against Elixir project

--- lib/green_validation/projects.ex
+++ lib/green_validation/projects.ex
@@ -7,36 +7,45 @@ defmodule GreenValidation.Projects do
 
   @projects [
     %Project{
-      name: "eex",
+      name: "elixir",
       repo_name: "elixir",
-      path: "lib/eex",
-      has_formatter_exs: false
-    },
-    # Commenting out Elixir for now as it needs extra work to compile and format
-    # %Project{name: "elixir", repo_name: "elixir", path: "lib/elixir", has_formatter_exs: false},
-    %Project{
-      name: "ex_unit",
-      repo_name: "elixir",
-      path: "lib/ex_unit",
-      has_formatter_exs: false
-    },
-    %Project{
-      name: "iex",
-      repo_name: "elixir",
-      path: "lib/iex",
-      has_formatter_exs: false
-    },
-    %Project{
-      name: "logger",
-      repo_name: "elixir",
-      path: "lib/logger",
-      has_formatter_exs: false
-    },
-    %Project{
-      name: "mix",
-      repo_name: "elixir",
-      path: "lib/mix",
-      has_formatter_exs: false
+      has_mix_exs: false,
+      environment: {__MODULE__, :elixir_environment},
+      rule_config: [
+        avoid_needless_pipelines: [except: [{"lib/elixir/test/elixir/code_normalizer/quoted_ast_test.exs", [471, 472, 636]}]],
+        true_in_cond: [except: [
+          "lib/elixir/test/elixir/kernel/expansion_test.exs",
+          {"lib/elixir/test/elixir/kernel/guard_test.exs", 421},
+          {"lib/elixir/lib/macro.ex", 2813},
+          "lib/elixir/test/elixir/module/types/expr_test.exs",
+          "lib/elixir/test/elixir/module/types/helpers_test.exs",
+          "lib/elixir/test/elixir/kernel/special_forms_test.exs",
+          "lib/elixir/test/elixir/fixtures/dialyzer/cond.ex"
+        ]],
+        upper_camel_case_for_modules: [except: [
+          "lib/mix/test/mix/tasks/compile.erlang_test.exs",
+          "lib/iex/test/iex/autocomplete_test.exs",
+          {"lib/elixir/test/elixir/map_test.exs", 493},
+          {"lib/elixir/test/elixir/kernel_test.exs", [913, 917]},
+          {"lib/elixir/test/elixir/module_test.exs", [277, 285]}
+        ]],
+        avoid_caps: [except: [
+          "lib/elixir/test/elixir/kernel/expansion_test.exs",
+          "lib/elixir/test/elixir/kernel/alias_test.exs",
+          {"lib/iex/test/iex/helpers_test.exs", 1561},
+          "lib/elixir/test/elixir/code_normalizer/quoted_ast_test.exs",
+          "lib/iex/test/iex/autocomplete_test.exs",
+          "lib/elixir/test/elixir/kernel/string_tokenizer_test.exs",
+          "lib/elixir/test/elixir/kernel/sigils_test.exs",
+          {"lib/iex/lib/iex/pry.ex", 576},
+          "lib/elixir/lib/kernel.ex",
+          "lib/elixir/lib/string.ex",
+          "lib/elixir/test/elixir/kernel/expansion_test.exs",
+          "lib/elixir/test/elixir/inspect_test.exs",
+          "lib/elixir/unicode/unicode.ex",
+          "lib/elixir/test/elixir/kernel/quote_test.exs"
+        ]]
+      ]
     },
     %Project{
       name: "phoenix",
@@ -77,5 +86,11 @@ defmodule GreenValidation.Projects do
       nil -> {:error, "Project not found: #{project_name}"}
       project -> {:ok, project}
     end
+  end
+
+  def elixir_environment(%Project{} = project) do
+    # For Elixir, we want to ensure that the PATH includes the local elixir bin directory
+    path = Project.path(project)
+    [{"PATH", "#{path}/bin:#{System.get_env("PATH")}"}]
   end
 end
