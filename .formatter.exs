fd38d0afa828a616b0edf18a62561899e47364c3
Joe Yates <joe.g.yates@gmail.com>
2026-03-06 11:52:50 +0100

    Include scripts in formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,4 @@
 [
-  inputs: ["{mix,.formatter}.exs", "{lib,test}/**/*.{ex,exs}"],
+  inputs: ["{mix,.formatter}.exs", "{lib,test}/**/*.{ex,exs}", "bin/*"],
   locals_without_parens: [assert: 1, refute: 1]
 ]
