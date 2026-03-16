4f00e23339b6b8a42dbe59576431a7330120620c
Joe Yates <joe.g.yates@gmail.com>
2026-03-16 21:23:38 +0100

    Hack: Set Logger format
    Need to migrate the scripts to Mix tasks, which would allow
    standard `config` configuration

--- bin/validate.exs
+++ bin/validate.exs
@@ -1,7 +1,9 @@
 #!/usr/bin/env elixir
 
-Mix.install([
-  {:green_validation, path: __DIR__ |> Path.join("..") |> Path.expand()}
-])
+Mix.install([{:green_validation, path: __DIR__ |> Path.join("..") |> Path.expand()}])
+
+Application.stop(:logger)
+Application.put_env(:logger, :console, format: "$time $metadata[$level] $message\n")
+{:ok, _} = Application.ensure_all_started(:logger)
 
 GreenValidation.CLI.Validate.main(System.argv())
